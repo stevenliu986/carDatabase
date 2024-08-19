0893e758944dc8e04e1413d9820cfb829d184597
StevenLiu <stevenliu986@gmail.com>
2024-08-19 19:46:53 +1000

    init

---
+++ src/main/java/com/packt/cardatabase/CarDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.packt.cardatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarDatabaseApplication.class, args);
+    }
+
+}
