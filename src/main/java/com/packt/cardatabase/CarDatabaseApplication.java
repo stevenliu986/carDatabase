3cb8186ef0f60614d7e35389e8f8ab392751af1e
StevenLiu <stevenliu986@gmail.com>
2024-08-19 19:48:59 +1000

    insert logging

--- src/main/java/com/packt/cardatabase/CarDatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CarDatabaseApplication.java
@@ -1,13 +1,18 @@
 package com.packt.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarDatabaseApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(CarDatabaseApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(CarDatabaseApplication.class, args);
+        logger.info("Car Database Application Started");
     }
 
 }
