beaf95d60249ba9c15c5f5585662774a238ce81f
StevenLiu <stevenliu986@gmail.com>
2024-08-21 21:14:24 +1000

    add new method

--- src/main/java/com/packt/cardatabase/domain/Car.java
+++ src/main/java/com/packt/cardatabase/domain/Car.java
@@ -43,6 +43,10 @@ public class Car {
         return id;
     }
 
+    public String getBrand() {
+        return brand;
+    }
+
     public void setBrand(String brand) {
         this.brand = brand;
     }
