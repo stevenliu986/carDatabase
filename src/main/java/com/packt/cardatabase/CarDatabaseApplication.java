aeec9803d55ca4677220e69355ff13070d0ab39b
StevenLiu <stevenliu986@gmail.com>
2024-08-22 13:21:45 +1000

    update car application

--- src/main/java/com/packt/cardatabase/CarDatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CarDatabaseApplication.java
@@ -2,6 +2,8 @@ package com.packt.cardatabase;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -14,9 +16,11 @@ public class CarDatabaseApplication implements CommandLineRunner {
     private static final Logger logger = LoggerFactory.getLogger(CarDatabaseApplication.class);
 
     private final CarRepository repository;
+    private final OwnerRepository ownerRepository;
 
-    public CarDatabaseApplication(CarRepository repository) {
+    public CarDatabaseApplication(CarRepository repository, OwnerRepository ownerRepository) {
         this.repository = repository;
+        this.ownerRepository = ownerRepository;
     }
 
     public static void main(String[] args) {
@@ -26,9 +30,14 @@ public class CarDatabaseApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000));
-        repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000));
-        repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000));
+
+        // Add owner objects and save these to db
+        Owner owner1 = new Owner("John" , "Johnson");
+        Owner owner2 = new Owner("Mary" , "Robinson");
+
+        repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+        repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner2));
+        repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000, owner2));
 
         // fetch all cars and log to console
         for (Car car : repository.findAll()) {
